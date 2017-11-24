9e4cebc8334fc9bf4258dfabd2ae5a71a18b2982
Jeff Cheng <jeff@JeffdeMacBook-Pro.local>
2017-11-24 17:25:58 +0800

    跨域支持

--- src/main/java/com/chengjf/admin/controller/LoginController.java
+++ src/main/java/com/chengjf/admin/controller/LoginController.java
@@ -4,12 +4,14 @@ import com.chengjf.admin.base.FrameResp;
 import com.chengjf.admin.component.user.model.User;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/api/user")
 @Slf4j
@@ -37,7 +39,7 @@ public class LoginController {
             Map<String, Object> map = Maps.newHashMap();
             map.put("name", "成建锋");
             map.put("avatar", "https://avatars0.githubusercontent.com/u/16893554?v=3&s=240");
-            map.put("age|20-25", 20);
+            map.put("age", 20);
             map.put("desc", "管理员就是我");
             return FrameResp.builder().code(0).msg("登录成功").data(map).build();
         } else {
